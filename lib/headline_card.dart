76968d720d78924e6c59da6eadcecd1731a970db
Paras-Sharma52 <parassharma929@gmail.com>
2022-09-21 17:34:23 +0530

    stable version

--- lib/headline_card.dart
+++ lib/headline_card.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:newsheadlines/news_detail.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Headlinecard extends StatefulWidget {
   Headlinecard({Key? key}) : super(key: key);
@@ -12,156 +13,216 @@ class Headlinecard extends StatefulWidget {
 }
 
 class _HeadlinecardState extends State<Headlinecard> {
+  var waiting = false;
   Future<void> fetchealine() async {
     final url = Uri.parse(
-      "https://newsapi.org/v2/everything?q=Apple&from=2022-08-18&sortBy=popularity&apiKey=0c2508c5f1bc4e9097dbb77b359b777e",
+      "https://newsapi.org/v2/everything?q=Apple&from=2022-09-21&sortBy=popularity&apiKey=d36156747a7d4d268e9d84d43886c735",
     );
+
     try {
       final response = await http.get(url);
       if (response.statusCode == 200) {
+        SharedPreferences preferences = await SharedPreferences.getInstance();
         final articles = json.decode(response.body) as Map<String, dynamic>;
-        var resBody = articles['articles'];
-        print(resBody);
-        return resBody;
+        var user = jsonEncode(articles);
+        preferences.setString('news', user);
+        info();
+        setState(() {
+          waiting = true;
+        });
       }
     } catch (e) {
       return print('noresponce');
     }
   }
 
+  Future<void> info() async {
+    try {
+      SharedPreferences preferences = await SharedPreferences.getInstance();
+
+      Map userarticles =
+          await jsonDecode(preferences.getString('news').toString());
+
+      var resBody = await userarticles['articles'];
+
+      // print(userarticles['articles']);
+      print(waiting);
+      return resBody;
+    } catch (e) {
+      print('data not available');
+    }
+  }
+
+  // @override
+  // void didChangeDependencies() {
+  //   // TODO: implement didChangeDependencies
+  //   waiting = true;
+  //   super.didChangeDependencies();
+  // }
+
   @override
   void initState() {
     // TODO: implement initState
     fetchealine();
+    info();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return FutureBuilder(
-        future: fetchealine(),
-        builder: ((context, AsyncSnapshot<dynamic> snapshot) {
-          if (snapshot.hasError) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: const Text('Something Went Wrong Please?'),
-                action: SnackBarAction(
-                  label: 'Retry',
-                  onPressed: () async {
-                    fetchealine();
-                  },
-                ),
-              ),
-            );
-          }
-          final newsArti = snapshot.data;
+    return waiting
+        ? FutureBuilder(
+            future: info(),
+            builder: ((context, AsyncSnapshot<dynamic> snapshot) {
+              final newsArti = snapshot.data;
 
-          return snapshot.connectionState == ConnectionState.waiting
-              ? const Center(
-                  child: CircularProgressIndicator(
-                  color: Colors.white,
-                ))
-              : RefreshIndicator(
-                  onRefresh: () async {
-                    await fetchealine();
-                  },
-                  child: ListView.builder(
-                      itemCount: 35,
-                      itemBuilder: ((context, index) {
-                        final articalSpec = newsArti[index];
-                        String date = articalSpec['publishedAt'];
-                        return GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: ((context) => Newsdetail(
-                                      artical: articalSpec,
-                                    )),
-                              ),
-                            );
-                          },
-                          child: Container(
-                            margin: const EdgeInsets.all(10),
-                            height: size.height / 3 - 40,
-                            width: size.width,
-                            child: Card(
-                              clipBehavior: Clip.antiAlias,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(12)),
-                              child: Stack(
+              return snapshot.connectionState == ConnectionState.waiting
+                  ? const Center(
+                      child: CircularProgressIndicator(
+                      color: Colors.white,
+                    ))
+                  : RefreshIndicator(
+                      onRefresh: () async {
+                        await info();
+                      },
+                      child: snapshot.data == null
+                          ? Center(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
                                 children: [
-                                  Hero(
-                                    tag: articalSpec['urlToImage'],
-                                    child: Image.network(
-                                      articalSpec['urlToImage'],
-                                      // 'https://images.unsplash.com/photo-1657299143549-73fb118d68aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
-                                      height: size.height,
-                                      width: size.width,
-                                      fit: BoxFit.cover,
-                                    ),
+                                  const Text(
+                                    'No internet',
+                                    style: TextStyle(color: Colors.white),
                                   ),
-                                  Column(
-                                    mainAxisAlignment: MainAxisAlignment.end,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Padding(
-                                        padding:
-                                            const EdgeInsets.only(left: 10),
-                                        child: SingleChildScrollView(
-                                          scrollDirection: Axis.vertical,
-                                          child: Text(
-                                            articalSpec['title'],
-                                            style: const TextStyle(
+                                  ElevatedButton.icon(
+                                    onPressed: () async {
+                                      setState(() {
+                                        snapshot.data == null
+                                            ? const Center(
+                                                child:
+                                                    CircularProgressIndicator(
                                                 color: Colors.white,
-                                                fontFamily: 'RobotoSlab',
-                                                fontSize: 20),
-                                          ),
-                                        ),
-                                      ),
-                                      Row(
-                                        children: [
-                                          Padding(
-                                            padding: const EdgeInsets.fromLTRB(
-                                                10, 24, 0, 12),
-                                            child: Text(
-                                              articalSpec['source']['name'],
-                                              style: const TextStyle(
-                                                  color: Colors.white,
-                                                  fontFamily:
-                                                      'RobotoSlab-Regular',
-                                                  fontStyle: FontStyle.normal,
-                                                  fontWeight: FontWeight.normal,
-                                                  fontSize: 12),
-                                            ),
-                                          ),
-                                          Padding(
-                                            padding: const EdgeInsets.fromLTRB(
-                                                10, 24, 0, 12),
-                                            child: Text(
-                                              date.substring(0, 10),
-                                              style: const TextStyle(
-                                                  color: Colors.grey,
-                                                  fontFamily:
-                                                      'RobotoSlab-Regular',
-                                                  fontStyle: FontStyle.normal,
-                                                  fontWeight: FontWeight.normal,
-                                                  fontSize: 12),
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ],
-                                  )
+                                              ))
+                                            : info();
+                                      });
+                                    },
+                                    icon: const Icon(Icons.refresh),
+                                    label: const Text('Retry'),
+                                  ),
                                 ],
                               ),
-                            ),
-                          ),
-                        );
-                      })),
-                );
-        }));
+                            )
+                          : ListView.builder(
+                              itemCount: 35,
+                              itemBuilder: ((context, index) {
+                                final articalSpec = newsArti[index];
+                                String date = articalSpec['publishedAt'];
+                                return GestureDetector(
+                                  onTap: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: ((context) => Newsdetail(
+                                              artical: articalSpec,
+                                            )),
+                                      ),
+                                    );
+                                  },
+                                  child: Container(
+                                    margin: const EdgeInsets.all(10),
+                                    height: size.height / 3 - 40,
+                                    width: size.width,
+                                    child: Card(
+                                      clipBehavior: Clip.antiAlias,
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(12)),
+                                      child: Stack(
+                                        children: [
+                                          Hero(
+                                            tag: articalSpec['urlToImage'],
+                                            child: Image.network(
+                                              articalSpec['urlToImage'],
+                                              height: size.height,
+                                              width: size.width,
+                                              fit: BoxFit.cover,
+                                            ),
+                                          ),
+                                          Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.end,
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              Padding(
+                                                padding: const EdgeInsets.only(
+                                                    left: 10),
+                                                child: SingleChildScrollView(
+                                                  scrollDirection:
+                                                      Axis.vertical,
+                                                  child: Text(
+                                                    articalSpec['title'],
+                                                    style: const TextStyle(
+                                                        color: Colors.white,
+                                                        fontFamily:
+                                                            'RobotoSlab',
+                                                        fontSize: 20),
+                                                  ),
+                                                ),
+                                              ),
+                                              Row(
+                                                children: [
+                                                  Padding(
+                                                    padding: const EdgeInsets
+                                                            .fromLTRB(
+                                                        10, 24, 0, 12),
+                                                    child: Text(
+                                                      articalSpec['source']
+                                                          ['name'],
+                                                      style: const TextStyle(
+                                                          color: Colors.white,
+                                                          fontFamily:
+                                                              'RobotoSlab-Regular',
+                                                          fontStyle:
+                                                              FontStyle.normal,
+                                                          fontWeight:
+                                                              FontWeight.normal,
+                                                          fontSize: 12),
+                                                    ),
+                                                  ),
+                                                  Padding(
+                                                    padding: const EdgeInsets
+                                                            .fromLTRB(
+                                                        10, 24, 0, 12),
+                                                    child: Text(
+                                                      date.substring(0, 10),
+                                                      style: const TextStyle(
+                                                          color: Colors.grey,
+                                                          fontFamily:
+                                                              'RobotoSlab-Regular',
+                                                          fontStyle:
+                                                              FontStyle.normal,
+                                                          fontWeight:
+                                                              FontWeight.normal,
+                                                          fontSize: 12),
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
+                                            ],
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                );
+                              })),
+                    );
+            }))
+        : const Center(
+            child: CircularProgressIndicator(
+            color: Colors.white,
+          ));
   }
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,24 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:newsheadlines/headline_card.dart';
 
 class Homepage extends StatelessWidget {
-  const Homepage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
+    // Future.delayed(const Duration(microseconds: 2000), () {
+    //   waiting = true;
+    // });
     return Scaffold(
-      backgroundColor: Color.fromARGB(221, 48, 42, 42),
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: const Text(
-          'H E A D L I N E S',
-          style: TextStyle(
-            color: Colors.white,
-            fontFamily: 'RobotoSlab',
+        backgroundColor: Color.fromARGB(221, 48, 42, 42),
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: const Text(
+            'H E A D L I N E S',
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'RobotoSlab',
+            ),
           ),
+          centerTitle: true,
         ),
-        centerTitle: true,
-      ),
-      body: Headlinecard(),
-    );
+        body: Headlinecard()
+        // : const Center(
+        //     child: CircularProgressIndicator(
+        //     color: Colors.white,
+        //   )),
+        );
   }
 }
